6b56d0556a00927aae895f63c4bef939eaf773bb
vb8118 <varunix@gmail.com>
2023-10-22 20:11:25 +0530

    smartDL and Wget modules downloaded

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from pySmartDL import SmartDL
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+url = "https://testfileorg.netwet.net/testfile.org-1GB-%20Corrupt.zip"
+output_file = "1GB-testlargefile.zip"
 
+dl = SmartDL(url, output_file)
+dl.start()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
